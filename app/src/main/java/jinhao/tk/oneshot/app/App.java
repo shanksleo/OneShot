7528ecdce605b3efd81f781a96cf6528032eaac0
shanks hu <hujinhao@bianfeng.com>
2016-12-22 15:06:13 +0800

    view util

---
+++ app/src/main/java/jinhao/tk/oneshot/app/App.java
@@ -0,0 +1,14 @@
+package jinhao.tk.oneshot.app;
+
+import android.app.Application;
+
+/**
+ * Created by shanks on 2016/12/22.
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ common_utils/src/main/java/jinhao/tk/commonutils/view/NoDoubleClickListener.java
@@ -0,0 +1,28 @@
+package jinhao.tk.commonutils.view;
+
+import android.view.View;
+
+import java.util.Calendar;
+
+
+/**
+ * Created by shanks on 16/9/20.
+ * 防止快速点击
+ */
+
+public abstract class NoDoubleClickListener implements View.OnClickListener {
+
+    public static final int MIN_CLICK_DELAY_TIME = 1000;
+    private long lastClickTime = 0;
+
+    @Override
+    public void onClick(View v) {
+        long currentTime = Calendar.getInstance().getTimeInMillis();
+        if (currentTime - lastClickTime > MIN_CLICK_DELAY_TIME) {
+            lastClickTime = currentTime;
+            onNoDoubleClick(v);
+        }
+    }
+
+    protected abstract void onNoDoubleClick(View v);
+}
