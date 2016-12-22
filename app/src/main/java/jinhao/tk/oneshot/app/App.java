c5045f1634adf7168abeff6751d09c899db74d88
shanks hu <hujinhao@bianfeng.com>
2016-12-22 19:35:21 +0800

    崩溃管理

--- app/src/main/java/jinhao/tk/oneshot/app/App.java
+++ app/src/main/java/jinhao/tk/oneshot/app/App.java
@@ -1,14 +1,28 @@
 package jinhao.tk.oneshot.app;
 
 import android.app.Application;
+import android.content.Context;
+
+import jinhao.tk.commonutils.common.CrashHandler;
 
 /**
  * Created by shanks on 2016/12/22.
  */
 
 public class App extends Application {
+
+    public static Context ctx;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        ctx = getApplicationContext();
+        CrashHandler.getInstance().init(getApplicationContext());
+
+
+    }
+
+    public static Context getInstance() {
+        return ctx;
     }
 }
